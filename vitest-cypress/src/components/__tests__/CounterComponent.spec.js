07e0b48be6359f209b7435c6eee83c4622823143
Clem's <115803908+KiSsWave@users.noreply.github.com>
2025-01-20 16:22:16 +0100

    vitest

--- vitest-cypress/src/components/__tests__/CounterComponent.spec.js
+++ vitest-cypress/src/components/__tests__/CounterComponent.spec.js
@@ -1,8 +1,8 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect, vi } from 'vitest';
+import { mount } from '@vue/test-utils';
+import { createTestingPinia } from '@pinia/testing';
+import Counter from '../CounterComponent.vue';
 
-import { mount } from '@vue/test-utils'
-import { createTestingPinia } from '@pinia/testing'
-import Counter from '../CounterComponent.vue'
 
 function mountCounter(x = 0) {
   const wrapper = mount(Counter, {
@@ -11,22 +11,46 @@ function mountCounter(x = 0) {
         createTestingPinia({
           createSpy: vi.fn,
           initialState: {
-            counter: { count: x } // start the counter at x instead of 0
-          }
-        })
-      ]
-    }
-  })
-  return wrapper
+            counter: { count: x },
+          },
+        }),
+      ],
+    },
+  });
+  return wrapper;
 }
 
 describe('Counter', () => {
   it('renders properly', () => {
-    const wrapper = mountCounter(50)
-    expect(wrapper.text()).toContain('Counter: 50')
-  })
+    const wrapper = mountCounter(50);
+    expect(wrapper.text()).toContain('Counter: 50');
+  });
+
   describe('Clicks', () => {
-    it('increments counter', () => {})
-    it('decrements counter', () => {})
-  })
-})
+    it('increments counter', async () => {
+      const wrapper = mountCounter(10);
+      const store = wrapper.vm.counterStore;
+
+      // Simuler un clic sur le bouton Increment
+      const incrementButton = wrapper.find('button:nth-child(2)');
+      await incrementButton.trigger('click');
+
+
+      expect(store.increment).toHaveBeenCalled();
+      expect(store.count).toBe(11);
+    });
+
+    it('decrements counter', async () => {
+      const wrapper = mountCounter(10);
+      const store = wrapper.vm.counterStore;
+
+
+      const decrementButton = wrapper.find('button:nth-child(1)');
+      await decrementButton.trigger('click');
+
+
+      expect(store.decrement).toHaveBeenCalled();
+      expect(store.count).toBe(9);
+    });
+  });
+});
